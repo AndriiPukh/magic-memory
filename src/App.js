e1a999f0607326136a851e013cba982b9a667bbc
AndriiPukh <puschock.andrey@gmail.com>
2022-04-30 10:38:13 +0300

    [MM-02] Add cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+const cardImages = [
+  { src: '/img/lion1.png' },
+  { src: '/img/lion3.png' },
+  { src: '/img/lion4.png' },
+  { src: '/img/lion5.png' },
+  { src: '/img/lion6.png' },
+  { src: '/img/lion7.png' },
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  console.log('turns', turns)
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button type="button">New Game</button>
+      <button type="button" onClick={shuffleCards}>
+        New Game
+      </button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="card-front" src={card.src} alt="Card front" />
+              <img
+                className="card-back"
+                src="/img/pngegg.png"
+                alt="Cart back"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
